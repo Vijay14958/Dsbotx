333ec1b751b00307cc0b1565828e01d2fa0b4e1f
Neha <116162193+Vijay14958@users.noreply.github.com>
2023-05-23 15:27:41 +0530

    Update info.py

--- info.py
+++ info.py
@@ -26,9 +26,9 @@ ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001574159030 -1001354385299 -1001662423682 -1001775308909').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-auth_channel = environ.get('AUTH_CHANNEL')
+auth_channel = environ.get('AUTH_CHANNEL', '-1001710985956')
 auth_grp = environ.get('AUTH_GROUP')
-AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else "-1001710985956"
+AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
